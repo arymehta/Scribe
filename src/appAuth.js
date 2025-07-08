fb64ff32f1a743e0f75a7b6a81fab6faa2f2d9ad
HardikMutha <hardikmutha2005@gmail.com>
2025-07-08 17:40:23 +0530

    Fix : relative path name

--- src/appAuth.js
+++ src/appAuth.js
@@ -1,15 +1,16 @@
-import { Octokit } from '@octokit/rest';
-import { createAppAuth } from '@octokit/auth-app';
-import fs from 'fs';
+import { Octokit } from "@octokit/rest";
+import { createAppAuth } from "@octokit/auth-app";
+import fs from "fs";
+import path from "path";
 
 export const createOctokitClient = (installationId) => {
-  const privateKey = fs.readFileSync('/home/amehta/Desktop/DocBot/github-app-private-key.pem', 'utf8');
-  // console.log(privateKey);
+  const filePath = path.join(__dirname, "github-app-private-key.pem");
+  const privateKey = fs.readFileSync(filePath, "utf8");
   const octokit = new Octokit({
     authStrategy: createAppAuth,
     auth: {
       appId: process.env.APP_ID,
-      privateKey, // correct format with newlines preserved
+      privateKey: privateKey,
       installationId,
     },
   });
