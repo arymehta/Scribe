12bba5b4413dd3c69fb4b6b83740f33ddcb22774
arymehta <amehta350@gmail.com>
2025-07-08 17:47:04 +0530

    fix: path naming in auth

--- src/appAuth.js
+++ src/appAuth.js
@@ -2,9 +2,15 @@ import { Octokit } from "@octokit/rest";
 import { createAppAuth } from "@octokit/auth-app";
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
 
 export const createOctokitClient = (installationId) => {
-  const filePath = path.join(__dirname, "github-app-private-key.pem");
+  
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const filePath = path.join(__dirname, "../", "github-app-private-key.pem");
   const privateKey = fs.readFileSync(filePath, "utf8");
   const octokit = new Octokit({
     authStrategy: createAppAuth,
