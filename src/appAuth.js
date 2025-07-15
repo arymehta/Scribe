0310a56a67183b458faad36bf95e88a3942574b9
HardikMutha <hardikmutha2005@gmail.com>
2025-07-15 16:23:57 +0530

    Fix : docker mutliline .env issue

--- src/appAuth.js
+++ src/appAuth.js
@@ -2,7 +2,7 @@ import { Octokit } from "@octokit/rest";
 import { createAppAuth } from "@octokit/auth-app";
 
 export const createOctokitClient = (installationId) => {
-  const privateKey = process.env.GITHUB_SECRET;
+  const privateKey = Buffer.from(process.env.GITHUB_SECRET, "base64").toString("utf-8");
   const octokit = new Octokit({
     authStrategy: createAppAuth,
     auth: {
